15cd5ad6ce18a32bca3707d24b7188abac844861
peterliu502 <798551899@qq.com>
2020-02-14 01:06:13 +0800

    1. 修改了了20200201_01_字符串与编码.py的str格式utf-8编码转Unicode字符部分代码 2. 修改了readme.md中的关键词式样，实现全部标签图片化

--- 20200201_01_字符串与编码.py
+++ 20200201_01_字符串与编码.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import datetime
+import codecs
 
 print("ord()函数:")  # ord()函数
 print('函数返回值数据类型:', type(ord('中')), sep='\n')  # ord()函数返回的是int类型
@@ -60,6 +61,22 @@ print('errors测试:')
 print(b'\xe4\xb8\xad\xe6\x96\x87\xff'.decode("utf-8", 'ignore'))  # 对无法编码的部分进行忽略
 print(b'\xe4\xb8\xad\xe6\x96\x87\xff'.decode("utf-8", 'replace'))  # 对无法编码的部分用�代替
 print(b'\xe4\xb8\xad\xe6\x96\x87\xff'.decode("utf-8", "backslashreplace"), end='\n\n')  # 对无法编码的部分用XML字符引用代替
+
+print('str格式utf-8编码转Unicode字符')
+print('方法1：')
+str1 = r'\xE5\x87\xBD\xE6\x95\xB0'
+# print(str1)
+bytes1 = str1.encode('utf-8')
+# print(bytes1)
+bytes2 = codecs.escape_decode(bytes1, 'hex-escape')
+print(bytes2[0].decode())
+print('方法2：')
+str1 = r'\xE5\x87\xBD\xE6\x95\xB0'.replace(r'\x', '')
+# print(str1)
+bytes1 = codecs.decode(str1, "hex")
+# print(bytes1)
+bytes2 = bytes1.decode()
+print(bytes2, end='\n\n')
 
 print('占位符%测试')
 # 格式 : %[- + 0 宽度.精度]类型码
